6d7b92d0a8bd76dd82ee0867fbfd15bfafaec4c0
Nico Georges <ngeorges@live.com>
2021-09-16 00:13:48 -0300

    Optimize menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,5 +1,5 @@
 <li class="side-menus {{ Request::is('/') ? 'active' : '' }}">
-    <a class="nav-link" href="/">
+    <a class="nav-link" href="{{ url('/') }}">
         <i class=" fas fa-building"></i><span>Dashboard</span>
     </a>
 </li>
@@ -8,10 +8,10 @@
     <i class="fas fa-address-card"></i><span>Clients</span>
     </a>
     <ul class="dropdown-menu">
-                <li class="{{ Request::is('clients') ? 'active' : '' }}"><a class="nav-link" href="/clients">List</a></li>
-                <li class="{{ Request::is('clients/creditcards') ? 'active' : '' }}"><a class="nav-link" href="/clients/creditcards">Credit Cards</a></li>
-                <li class="{{ Request::is('clients/import') ? 'active' : '' }}"><a class="nav-link" href="/clients/import">Import</a></li>
-                <li class="{{ Request::is('clients/import-logs') ? 'active' : '' }}"><a class="nav-link" href="/clients/import-logs">Import Logs</a></li>
+                <li class="{{ Request::is('clients') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/clients') }}">List</a></li>
+                <li class="{{ Request::is('clients/creditcards') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/clients/creditcards') }}">Credit Cards</a></li>
+                <li class="{{ Request::is('clients/import') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/clients/import') }}">Import</a></li>
+                <li class="{{ Request::is('clients/import-logs') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/clients/import-logs') }}">Import Logs</a></li>
               </ul>
 </li>
 <li class="side-menus {{ Request::is('users/*') ? 'active' : '' }}">
@@ -19,7 +19,7 @@
         <i class=" fas fa-user"></i><span>Users</span>
     </a>
     <ul class="dropdown-menu">
-                <li class="{{ Request::is('users/list') ? 'active' : '' }}"><a class="nav-link" href="/users/list">List</a></li>
-                <li class="{{ Request::is('users/add') ? 'active' : '' }}"><a class="nav-link" href="/users/add">Add User</a></li>
+                <li class="{{ Request::is('users/list') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/users/list') }}">List</a></li>
+                <li class="{{ Request::is('users/add') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/users/add') }}">Add User</a></li>
               </ul>
 </li>
